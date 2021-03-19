1c6c50050fa7a06a451c9458664f3231c97dcd38
Lucas Almeida <lpalmeidabh@gmail.com>
2021-03-19 01:27:11 -0300

    Initial commit

---
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,22 @@
+import { Response, Request } from "express";
+
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+class CreateUserController {
+  constructor(private createUserUseCase: CreateUserUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    const { name, email } = request.body;
+
+    try {
+      const user = this.createUserUseCase.execute({ email, name });
+      return response
+        .status(201)
+        .json({ admin: user.admin, email: user.email, name: user.name });
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
+  }
+}
+
+export { CreateUserController };
---
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -0,0 +1,22 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class TurnUserAdminUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User {
+    let user = this.usersRepository.findById(user_id);
+    if (!user) {
+      throw new Error("User not Found");
+    }
+
+    user = this.usersRepository.turnAdmin(user);
+    return user;
+  }
+}
+
+export { TurnUserAdminUseCase };
